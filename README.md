3197b2bcf2c0a471acdac325c13df708d1d954cf
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-11-22 14:00:13 +0000

    Final commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Training Days
+## Project #26 from Codecademy's Full Stack Engineer Career Path
+### By Anton Vlasenko (AntonV0)  
+## Project Outline
+Training Days is a simple program for running athletes that gives them a random event (marathon, triathlon, or pentathlon) and their time to train for it (50, 100, or 200 days). The aim of this project is to fix the wrong scope of the variables to make the program run properly, as shown in the starter code.
+## What I Learned
+  - The global and local scopes of variables and their importance in maintenance and debugging
+  - Causes of ReferenceError
+***
+*Please note that in each project, the first commit is always the starter code provided by Codecademy.*
--- main.js
+++ main.js
@@ -1,44 +1,52 @@
-// The scope of `random` is too loose 
-const random = Math.floor(Math.random() * 3);
-
+// The scope of `random` is too loose
 const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
   if (random === 0) {
-    return 'Marathon';
+    return "Marathon";
   } else if (random === 1) {
-    return 'Triathlon';
+    return "Triathlon";
   } else if (random === 2) {
-    return 'Pentathlon';
+    return "Pentathlon";
   }
 };
 
-// The scope of `days` is too tight 
-const getTrainingDays = event => {
-
-  if (event === 'Marathon') {
-    let days = 50;
-  } else if (event === 'Triathlon') {
-    let days = 100;
-  } else if (event === 'Pentathlon') {
-    let days = 200;
+// The scope of `days` is too tight
+const getTrainingDays = (event) => {
+  let days;
+  if (event === "Marathon") {
+    days = 50;
+  } else if (event === "Triathlon") {
+    days = 100;
+  } else if (event === "Pentathlon") {
+    days = 200;
   }
 
   return days;
 };
 
-// The scope of `name` is too tight 
-const logEvent = event => {
-  const name = 'Nala';
+// The scope of `name` is too tight
+const name = "Nala";
+
+const logEvent = (name, event) => {
   console.log(`${name}'s event is: ${event}`);
 };
 
-const logTime = days => {
-  const name = 'Nala';
+const logTime = (name, days) => {
   console.log(`${name}'s time to train is: ${days} days`);
 };
 
 const event = getRandEvent();
 const days = getTrainingDays(event);
 
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-logEvent(event);
-logTime(days);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime
+logEvent(name, event);
+logTime(name, days);
+
+// Add another competitor
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = "Warren";
+
+logEvent(name2, event2);
+logTime(name2, days2);
+
